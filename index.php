7e3f47a3d3d6b095c9c1df5728af921e0e12b5eb
pweee <weephilmon@gmail.com>
2016-07-23 22:42:07 +0800

    capitalization

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
 	$A = $_POST['A'];
 	$B = $_POST['B'];
 	$C = $_POST['C'];
-	$ANS1 = (-$B+(sqrt(pow($B,2)-(4*$A*$C))))/(2*$A);
-	$ANS2 = (-$B-(sqrt(pow($B,2)-(4*$A*$C))))/(2*$A);
+	$ans1 = (-$B+(sqrt(pow($B,2)-(4*$A*$C))))/(2*$A);
+	$ans2 = (-$B-(sqrt(pow($B,2)-(4*$A*$C))))/(2*$A);
 	echo $ans1;
 	echo $ans2;
 		?>
