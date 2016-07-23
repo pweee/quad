8ac257bdc94bea3d37317ba5cf9540da90df5f5b
pweee <weephilmon@gmail.com>
2016-07-23 22:52:35 +0800

    minor php changes

--- index.php
+++ index.php
@@ -9,8 +9,8 @@
 	$C = $_POST['C'];
 	$ans1 = (-$B+(sqrt(pow($B,2)-(4*$A*$C))))/(2*$A);
 	$ans2 = (-$B-(sqrt(pow($B,2)-(4*$A*$C))))/(2*$A);
-	echo $ans1;
-	echo $ans2;
+	echo "x1= " . $ans1;
+	echo "x2= " . $ans2;
 		?>
 </body>
 </html>
