2dd2820cdbbc123c0d237d3db47549216b16be4e
pweee <weephilmon@gmail.com>
2017-08-24 16:49:52 +0800

    get

--- hellophp.php
+++ hellophp.php
@@ -4,9 +4,9 @@
 	<title>Information Gathered</title>
 </head>
 	<?php
-	$A = $_POST['A'];
-	$B = $_POST['B'];
-	$C = $_POST['C'];
+	$A = $_GET'A'];
+	$B = $_GET['B'];
+	$C = $_GET['C'];
 	$ANS1 = (-$B+(sqrt(pow($B,2)-(4*$A*$C))))/(2*$A);
 	$ANS2 = (-$B-(sqrt(pow($B,2)-(4*$A*$C))))/(2*$A);
 	echo $ANS1."<br>";
